e553a83a758ff6bd10e32d1a5b1becf83631995b
Isma <122747777+CacaBueno64@users.noreply.github.com>
2024-10-30 18:55:28 +0100

    Add files via upload

---
+++ animation/bonebool.py
@@ -0,0 +1,17 @@
+import struct
+class BoneBool:
+    def __init__(self, X):
+        self.X = X
+        
+    def __str__(self):
+        return f"({self.X})"
+    
+    def __eq__(self, obj):
+        same_x = self.X == obj.X
+        if (same_x == False):
+            return False
+        else:
+            return True
+    
+    def ToBytes(self):
+        return struct.pack("<B", float(self.X))
---
+++ animation/bonelocation.py
@@ -0,0 +1,21 @@
+import struct
+class BoneLocation:
+    def __init__(self, X, Y, Z):
+        self.X = X
+        self.Y = Y
+        self.Z = Z
+    
+    def __str__(self):
+        return f"({self.X}, {self.Y}, {self.Z})"
+    
+    def __eq__(self, obj):
+        same_x = self.X == obj.X
+        same_y = self.Y == obj.Y
+        same_z = self.Z == obj.Z
+        if (same_x == False or same_y == False or same_z == False):
+            return False
+        else:
+            return True
+    
+    def ToBytes(self):
+        return struct.pack("<fff", float(self.X), float(self.Y), float(self.Z))
