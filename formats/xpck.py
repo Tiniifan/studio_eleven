6055b0a79b068ad4ff0dbf048f7f9741c2b5df07
Isma <122747777+CacaBueno64@users.noreply.github.com>
2025-03-21 00:14:02 +0100

    Update xpck.py

--- formats/xpck.py
+++ formats/xpck.py
@@ -1,7 +1,7 @@
 import os
 import math
 import zlib
-import struct
+from struct import pack, unpack
 from ..compression import *
 
 def file_count_to_hex(file_count):
@@ -45,48 +45,81 @@ def open_file(file_item):
     else:
         raise ValueError("Unsupported input type. Please provide a filename or a bytearray.")
     
-    header = struct.unpack("4s", data[:4])[0].decode()
-    if header != "XPCK":
-        raise Exception("File header error")
-
-    file_count = struct.unpack("<H", data[4:6])[0] & 0xFFF
-    file_info_offset = struct.unpack("<H", data[6:8])[0] * 4
-    file_table_offset = struct.unpack("<H", data[8:10])[0] * 4
-    data_offset = struct.unpack("<H", data[10:12])[0] * 4
-    filename_table_size = struct.unpack("<H", data[14:16])[0] * 4
-
-    hash_to_data = {}
-    for i in range(file_count):
-        name_crc = struct.unpack("<I", data[file_info_offset + i * 12 : file_info_offset + i * 12 + 4])[0]
-        offset = struct.unpack("<H", data[file_info_offset + i * 12 + 6 : file_info_offset + i * 12 + 8])[0]
-        size = struct.unpack("<H", data[file_info_offset + i * 12 + 8 : file_info_offset + i * 12 + 10])[0]
-        offset_ext = struct.unpack("<B", data[file_info_offset + i * 12 + 10 : file_info_offset + i * 12 + 11])[0]
-        size_ext = struct.unpack("<B", data[file_info_offset + i * 12 + 11 : file_info_offset + i * 12 + 12])[0]
-
-        offset |= offset_ext << 16
-        size |= size_ext << 16
-        offset = offset * 4 + data_offset
-
-        file_data = data[offset : offset + size]
-
-        hash_to_data[name_crc] = file_data
-
-    name_table = data[file_table_offset : file_table_offset + filename_table_size]
-    name_table = compressor.decompress(name_table)
-
-    pos = 0
-    for i in range(file_count):
-        name_length = name_table.find(b'\x00', pos)
-        name = name_table[pos:name_length].decode("utf-8")
-        pos = name_length + 1
-
-        crc = zlib.crc32(name.encode("utf-8"))
-        if crc in hash_to_data:
-            files[name] = hash_to_data[crc]
-        else:
-            print("Couldn't find", name, hex(crc))
-
-    return files
+    files = {}
+    
+    magic = data[:4]
+    if magic == b"XFSP":
+        file_count = unpack("<H", data[4:6])[0] & 0xFFF
+        file_info_offset = unpack("<H", data[6:8])[0] * 4
+        file_table_offset = unpack("<H", data[8:10])[0] * 4
+        data_offset = unpack("<H", data[10:12])[0] * 4
+        filename_table_size = unpack("<H", data[14:16])[0] * 4
+        
+        name_table = data[file_table_offset : file_table_offset + filename_table_size]
+        name_table = compressor.decompress(name_table)
+        
+        for i in range(file_count):
+            unk = unpack("<H", data[file_info_offset + i * 10 + 0 : file_info_offset + i * 10 + 2])[0]
+            name_offset = unpack("<H", data[file_info_offset + i * 10 + 2: file_info_offset + i * 10 + 4])[0]
+            offset = unpack("<H", data[file_info_offset + i * 10 + 4 : file_info_offset + i * 10 + 6])[0]
+            size = unpack("<H", data[file_info_offset + i * 10 + 6 : file_info_offset + i * 10 + 8])[0]
+            offset_ext = unpack("<B", data[file_info_offset + i * 10 + 8 : file_info_offset + i * 10 + 9])[0]
+            size_ext =  unpack("<B", data[file_info_offset + i * 10 + 9 : file_info_offset + i * 10 + 10])[0]
+            
+            offset |= offset_ext << 16
+            size |= size_ext << 16
+            offset = offset * 4 + data_offset
+            
+            file_data = data[offset : offset + size]
+            
+            # Get name
+            name_length = name_table.find(b'\x00', name_offset)
+            name = name_table[name_offset:name_length].decode("utf-8")
+            
+            files[name] = file_data
+            
+        return files
+    elif magic == b"XPCK":
+        file_count = unpack("<H", data[4:6])[0] & 0xFFF
+        file_info_offset = unpack("<H", data[6:8])[0] * 4
+        file_table_offset = unpack("<H", data[8:10])[0] * 4
+        data_offset = unpack("<H", data[10:12])[0] * 4
+        filename_table_size = unpack("<H", data[14:16])[0] * 4
+        
+        hash_to_data = {}
+        for i in range(file_count):
+            name_crc = unpack("<I", data[file_info_offset + i * 12 : file_info_offset + i * 12 + 4])[0]
+            offset = unpack("<H", data[file_info_offset + i * 12 + 6 : file_info_offset + i * 12 + 8])[0]
+            size = unpack("<H", data[file_info_offset + i * 12 + 8 : file_info_offset + i * 12 + 10])[0]
+            offset_ext = unpack("<B", data[file_info_offset + i * 12 + 10 : file_info_offset + i * 12 + 11])[0]
+            size_ext = unpack("<B", data[file_info_offset + i * 12 + 11 : file_info_offset + i * 12 + 12])[0]
+            
+            offset |= offset_ext << 16
+            size |= size_ext << 16
+            offset = offset * 4 + data_offset
+            
+            file_data = data[offset : offset + size]
+            
+            hash_to_data[name_crc] = file_data
+        
+        name_table = data[file_table_offset : file_table_offset + filename_table_size]
+        name_table = compressor.decompress(name_table)
+        
+        pos = 0
+        for i in range(file_count):
+            name_length = name_table.find(b'\x00', pos)
+            name = name_table[pos:name_length].decode("utf-8")
+            pos = name_length + 1
+            
+            crc = zlib.crc32(name.encode("utf-8"))
+            if crc in hash_to_data:
+                files[name] = hash_to_data[crc]
+            else:
+                print("Couldn't find", name, hex(crc))
+        
+        return files
+    else:
+        raise Exception(f"Unknown xc magic: {magic}")
 
 def pack(files, output_file):
     offset = 0
@@ -111,19 +144,19 @@ def pack(files, output_file):
 
     # Writes XPCK file header
     with open(output_file, 'wb') as file:
-        file.write(struct.pack("4s", "XPCK".encode()))
-        file.write(struct.pack("<H", file_count_to_hex(len(files))))
-        file.write(struct.pack("<H", 20 // 4))
-        file.write(struct.pack("<H", (20 + len(files) * 12) // 4))
-        file.write(struct.pack("<H", (20 + len(files) * 12 + len(compressed_name_table)) // 4))
-        file.write(struct.pack("<H", len(files) * 12 // 4))
-        file.write(struct.pack("<H", len(compressed_name_table) // 4))
+        file.write(pack("4s", "XPCK".encode()))
+        file.write(pack("<H", file_count_to_hex(len(files))))
+        file.write(pack("<H", 20 // 4))
+        file.write(pack("<H", (20 + len(files) * 12) // 4))
+        file.write(pack("<H", (20 + len(files) * 12 + len(compressed_name_table)) // 4))
+        file.write(pack("<H", len(files) * 12 // 4))
+        file.write(pack("<H", len(compressed_name_table) // 4))
 
         data_size = 0
         for data in files.values():
             data_size += len(data["data"])
             
-        file.write(struct.pack("<I", offset // 4))
+        file.write(pack("<I", offset // 4))
 
         # Sort file name by crc32
         key_crc32 = {}
@@ -143,12 +176,12 @@ def pack(files, output_file):
             size_higher = file_size & 0xFFFF
             size_lower = file_size >> 16
             
-            file.write(struct.pack("<I", name_crc))
-            file.write(struct.pack("<H", files[filename]["name_offset"]))
-            file.write(struct.pack("<H", offset_higher))
-            file.write(struct.pack("<H", size_higher))
-            file.write(struct.pack("<B", offset_lower))
-            file.write(struct.pack("<B", size_lower))
+            file.write(pack("<I", name_crc))
+            file.write(pack("<H", files[filename]["name_offset"]))
+            file.write(pack("<H", offset_higher))
+            file.write(pack("<H", size_higher))
+            file.write(pack("<B", offset_lower))
+            file.write(pack("<B", size_lower))
 
         # Write name table
         file.write(compressed_name_table)
