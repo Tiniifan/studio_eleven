6667d48943b14e9b83218efb2393cc1ca7423af9
Isma <122747777+CacaBueno64@users.noreply.github.com>
2025-04-06 22:28:49 +0200

    Update xcma.py

--- formats/xcma.py
+++ formats/xcma.py
@@ -174,7 +174,13 @@ def write(animation_name, camera_speed, cam_values):
         cam_data_stream = bytearray()
         cam_data_start_offset = 0
 
-        cam_data_stream += b'\xFF\xFF' + struct.pack('BB', len(cam_value), 0x20)
+        cam_data_stream += b'\xFF\xFF'
+        if (len(cam_value) < 255):
+            cam_data_stream += pack("<BB", len(cam_value), 0x00)
+        else:
+            lowFrameCount = len(cam_value) & 0xFF
+            highFrameCount = 32 + (len(cam_value) >> 8) & 0xFF
+            cam_data_stream += pack("<BB", lowFrameCount, highFrameCount)
 
         frames_indexes = [key for key in cam_value.keys()]
         cam_data_stream += struct.pack(f'{len(frames_indexes)}h', *frames_indexes)
